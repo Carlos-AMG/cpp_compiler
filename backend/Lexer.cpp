cc9c23396b32722de433e8a1eccc724e0e49bcad
Carlos-AMG <alberto-cmg@outlook.es>
2023-09-17 14:58:45 -0600

    added all tokens

--- backend/Lexer.cpp
+++ backend/Lexer.cpp
@@ -1,46 +1,165 @@
 #include "Lexer.h"
 #include "utils/utils.h"
 #include <iostream>
+#include <regex>
 
-int Lexer::analyze(std::string line){
-    int i, j, k;
+// int Lexer::analyze(std::string line){
+//     int i, j, k;
+//     int resultingTokens = 0;
+//     int lineLen = line.length();
+//     std::string tokenVal;
+//     for (i=0; i < lineLen; i++){
+//         if (isspace(line[i])){
+//             continue;
+//         }
+//         if (isdigit((line[i]))){
+//             std::string numericVal = "";
+//             numericVal += line[i];
+//             // int dot_count = 0; //for floats
+//             for (j=i+1; j < lineLen; j++){
+//                 if (isspace(line[j])){
+//                     continue;
+//                 }
+//                 if (!isdigit(line[j]))
+//                     break;
+//                 numericVal += line[j];
+//             }
+//             i = j - 1; // Move i to the last digit of the integer
+//             this->tokens.push_back(Token("INTEGER", numericVal));
+//             resultingTokens++;
+//         } else if (isOperator(line[i])){
+//             this->tokens.push_back(Token("OPERATOR", std::string(1, line[i])));
+//             resultingTokens++;
+//         }else {
+//             // error
+//             // pending error class
+//             std::cerr << "Lexical Error: character: '"<< line[i] << "' is not valid in the source code";
+//         }
+//     }
+//     return resultingTokens;
+// }
+
+
+// Define and initialize the static const regex members
+const std::regex Lexer::identifierRegex("[a-zA-Z][a-zA-Z0-9]*");
+const std::regex Lexer::integerRegex("[0-9]+");
+const std::regex Lexer::realRegex("[0-9]+\\.[0-9]+");
+const std::regex Lexer::additionOperatorRegex("[\\+\\-]");
+const std::regex Lexer::multiplicationOperatorRegex("[\\*/]");
+const std::regex Lexer::assignmentOperatorRegex("=");
+const std::regex Lexer::relationalOperatorRegex("[<>]=?|!=|==");
+const std::regex Lexer::logicalAndOperatorRegex("&&");
+const std::regex Lexer::logicalOrOperatorRegex("\\|\\|");
+const std::regex Lexer::logicalNotOperatorRegex("!");
+const std::regex Lexer::leftParenthesisRegex("\\(");
+const std::regex Lexer::rightParenthesisRegex("\\)");
+const std::regex Lexer::leftBraceRegex("\\{");
+const std::regex Lexer::rightBraceRegex("\\}");
+const std::regex Lexer::semicolonRegex(";");
+const std::regex Lexer::reservedWordsRegex("if|while|return|else|int|float");
+
+int Lexer::analyze(std::string line) {
+    int i = 0;
     int resultingTokens = 0;
     int lineLen = line.length();
-    std::string tokenVal;
-    for (i=0; i < lineLen; i++){
-        if (isspace(line[i])){
-            continue;
+
+    while (i < lineLen) {
+        // Skip consecutive whitespace
+        while (i < lineLen && isspace(line[i])) {
+            i++;
         }
-        if (isdigit((line[i]))){
-            std::string numericVal = "";
-            numericVal += line[i];
-            // int dot_count = 0; //for floats
-            for (j=i+1; j < lineLen; j++){
-                if (isspace(line[j])){
-                    continue;
-                }
-                if (!isdigit(line[j]))
-                    break;
-                numericVal += line[j];
+
+        if (i >= lineLen) {
+            // Reached the end of the line, exit the loop
+            break;
+        }
+
+        // Find the end of the current token
+        int lexemeEnd = i;
+
+        // Check if the current character is an operator
+        if (std::regex_match(std::string(1, line[i]), additionOperatorRegex) ||
+            std::regex_match(std::string(1, line[i]), multiplicationOperatorRegex) ||
+            std::regex_match(std::string(1, line[i]), assignmentOperatorRegex) ||
+            std::regex_match(std::string(1, line[i]), relationalOperatorRegex) ||
+            std::regex_match(std::string(1, line[i]), logicalAndOperatorRegex) ||
+            std::regex_match(std::string(1, line[i]), logicalOrOperatorRegex) ||
+            std::regex_match(std::string(1, line[i]), logicalNotOperatorRegex) ||
+            std::regex_match(std::string(1, line[i]), leftParenthesisRegex) ||
+            std::regex_match(std::string(1, line[i]), rightParenthesisRegex) ||
+            std::regex_match(std::string(1, line[i]), leftBraceRegex) ||
+            std::regex_match(std::string(1, line[i]), rightBraceRegex) ||
+            std::regex_match(std::string(1, line[i]), semicolonRegex)) {
+            
+            // If it's an operator, add it as a separate token
+            std::string currentOperator(1, line[i]);
+            this->tokens.push_back(Token("OPERATOR", currentOperator));
+            resultingTokens++;
+            i++; // Move to the next character
+        }
+        else {
+            // Find the end of the current token (lexemeEnd)
+            while (lexemeEnd < lineLen && !isspace(line[lexemeEnd]) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), additionOperatorRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), multiplicationOperatorRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), assignmentOperatorRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), relationalOperatorRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), logicalAndOperatorRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), logicalOrOperatorRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), logicalNotOperatorRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), leftParenthesisRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), rightParenthesisRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), leftBraceRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), rightBraceRegex) &&
+                !std::regex_match(std::string(1, line[lexemeEnd]), semicolonRegex)) {
+                lexemeEnd++;
             }
-            i = j - 1; // Move i to the last digit of the integer
-            this->tokens.push_back(Token("INTEGER", numericVal));
-            resultingTokens++;
-        } else if (isOperator(line[i])){
-            this->tokens.push_back(Token("OPERATOR", std::string(1, line[i])));
-            resultingTokens++;
-        }else {
-            // error
-            // pending error class
-            std::cerr << "Lexical Error: character: '"<< line[i] << "' is not valid in the source code";
+
+            // Extract the current token as a substring
+            std::string currentLexeme = line.substr(i, lexemeEnd - i);
+            std::cout << "lexeme: " << currentLexeme << std::endl;
+
+            if (std::regex_match(currentLexeme, reservedWordsRegex)) {
+                if (std::regex_search(currentLexeme, reservedWordsRegex)) {
+                    this->tokens.push_back(Token("RESERVED", currentLexeme));
+                    resultingTokens++;
+                } else {
+                    // Handle the case where there's no match (error)
+                    std::cerr << "Lexical Error: Unexpected characters starting with '" << line[i] << "'" << std::endl;
+                    // i++; // Skip the invalid character
+                }
+            }
+            else if (std::regex_match(currentLexeme, integerRegex)) {
+                this->tokens.push_back(Token("INTEGER", currentLexeme));
+                resultingTokens++;
+            }
+            else if (std::regex_match(currentLexeme, realRegex)) {
+                this->tokens.push_back(Token("FLOAT", currentLexeme));
+                resultingTokens++;
+            }
+            else if (std::regex_match(currentLexeme, identifierRegex)) {
+                this->tokens.push_back(Token("IDENTIFIER", currentLexeme));
+                resultingTokens++;
+            }
+            else {
+                // error
+                // pending error class
+                std::cerr << "Lexical Error: character: '" << line[i] << "' is not valid in the source code" << std::endl;
+                // i++; // Skip the invalid character
+            }
+
+            i = lexemeEnd;
         }
     }
+
     return resultingTokens;
 }
 
+
+
 void Lexer::printTokens(){
     for (auto token : this->tokens){
-        std::cout << "Token: " << token.name << ", Value: " << token.value << std::endl;
+        std::cout << "Token: " << token.name << ", Lexeme: " << token.value << std::endl;
     }
 }
 
--- backend/Lexer.h
+++ backend/Lexer.h
@@ -3,12 +3,31 @@
 
 #include <vector>
 #include "Token.h"
+#include <regex>
 
 struct Lexer{
     std::vector<Token> tokens;
     int analyze(std::string line);
     void printTokens();
     std::vector<Token> getTokens();
+
+
+    static const std::regex identifierRegex;
+    static const std::regex integerRegex;
+    static const std::regex realRegex;
+    static const std::regex additionOperatorRegex;
+    static const std::regex multiplicationOperatorRegex;
+    static const std::regex assignmentOperatorRegex;
+    static const std::regex relationalOperatorRegex;
+    static const std::regex logicalAndOperatorRegex;
+    static const std::regex logicalOrOperatorRegex;
+    static const std::regex logicalNotOperatorRegex;
+    static const std::regex leftParenthesisRegex;
+    static const std::regex rightParenthesisRegex;
+    static const std::regex leftBraceRegex;
+    static const std::regex rightBraceRegex;
+    static const std::regex semicolonRegex;
+    static const std::regex reservedWordsRegex;
 };
 
 #endif
