6440e502eb9319aae2a370933b8a9b7a81680a4a
Carlos-AMG <alberto-cmg@outlook.es>
2023-09-18 17:43:33 -0600

    fixed logical operators

--- backend/Lexer.cpp
+++ backend/Lexer.cpp
@@ -49,8 +49,8 @@ const std::regex Lexer::additionOperatorRegex("[\\+\\-]");
 const std::regex Lexer::multiplicationOperatorRegex("[\\*/]");
 const std::regex Lexer::assignmentOperatorRegex("=");
 const std::regex Lexer::relationalOperatorRegex("[<>]=?|!=|==");
-const std::regex Lexer::logicalAndOperatorRegex("&&");
-const std::regex Lexer::logicalOrOperatorRegex("\\|\\|");
+const std::regex Lexer::logicalAndOperatorRegex("&");
+const std::regex Lexer::logicalOrOperatorRegex("\\|");
 const std::regex Lexer::logicalNotOperatorRegex("!");
 const std::regex Lexer::leftParenthesisRegex("\\(");
 const std::regex Lexer::rightParenthesisRegex("\\)");
@@ -58,104 +58,6 @@ const std::regex Lexer::leftBraceRegex("\\{");
 const std::regex Lexer::rightBraceRegex("\\}");
 const std::regex Lexer::semicolonRegex(";");
 const std::regex Lexer::reservedWordsRegex("if|while|return|else|int|float");
-
-// int Lexer::analyze(std::string line) {
-//     int i = 0;
-//     int resultingTokens = 0;
-//     int lineLen = line.length();
-
-//     while (i < lineLen) {
-//         // Skip consecutive whitespace
-//         while (i < lineLen && isspace(line[i])) {
-//             i++;
-//         }
-
-//         if (i >= lineLen) {
-//             // Reached the end of the line, exit the loop
-//             break;
-//         }
-
-//         // Find the end of the current token
-//         int lexemeEnd = i;
-
-//         // Check if the current character is an operator
-//         if (std::regex_match(std::string(1, line[i]), additionOperatorRegex) ||
-//             std::regex_match(std::string(1, line[i]), multiplicationOperatorRegex) ||
-//             std::regex_match(std::string(1, line[i]), assignmentOperatorRegex) ||
-//             std::regex_match(std::string(1, line[i]), relationalOperatorRegex) ||
-//             std::regex_match(std::string(1, line[i]), logicalAndOperatorRegex) ||
-//             std::regex_match(std::string(1, line[i]), logicalOrOperatorRegex) ||
-//             std::regex_match(std::string(1, line[i]), logicalNotOperatorRegex) ||
-//             std::regex_match(std::string(1, line[i]), leftParenthesisRegex) ||
-//             std::regex_match(std::string(1, line[i]), rightParenthesisRegex) ||
-//             std::regex_match(std::string(1, line[i]), leftBraceRegex) ||
-//             std::regex_match(std::string(1, line[i]), rightBraceRegex) ||
-//             std::regex_match(std::string(1, line[i]), semicolonRegex)) {
-            
-//             // If it's an operator, add it as a separate token
-//             std::string currentOperator(1, line[i]);
-//             this->tokens.push_back(Token("OPERATOR", currentOperator));
-//             resultingTokens++;
-//             i++; // Move to the next character
-//         }
-//         else {
-//             // Find the end of the current token (lexemeEnd)
-//             while (lexemeEnd < lineLen && !isspace(line[lexemeEnd]) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), additionOperatorRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), multiplicationOperatorRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), assignmentOperatorRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), relationalOperatorRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), logicalAndOperatorRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), logicalOrOperatorRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), logicalNotOperatorRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), leftParenthesisRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), rightParenthesisRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), leftBraceRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), rightBraceRegex) &&
-//                 !std::regex_match(std::string(1, line[lexemeEnd]), semicolonRegex)) {
-//                 lexemeEnd++;
-//             }
-
-//             // Extract the current token as a substring
-//             std::string currentLexeme = line.substr(i, lexemeEnd - i);
-//             std::cout << "lexeme: " << currentLexeme << std::endl;
-
-//             if (std::regex_match(currentLexeme, reservedWordsRegex)) {
-//                 if (std::regex_search(currentLexeme, reservedWordsRegex)) {
-//                     this->tokens.push_back(Token("RESERVED", currentLexeme));
-//                     resultingTokens++;
-//                 } else {
-//                     // Handle the case where there's no match (error)
-//                     std::cerr << "Lexical Error: Unexpected characters starting with '" << line[i] << "'" << std::endl;
-//                     // i++; // Skip the invalid character
-//                 }
-//             }
-//             else if (std::regex_match(currentLexeme, integerRegex)) {
-//                 this->tokens.push_back(Token("INTEGER", currentLexeme));
-//                 resultingTokens++;
-//             }
-//             else if (std::regex_match(currentLexeme, realRegex)) {
-//                 this->tokens.push_back(Token("FLOAT", currentLexeme));
-//                 resultingTokens++;
-//             }
-//             else if (std::regex_match(currentLexeme, identifierRegex)) {
-//                 this->tokens.push_back(Token("IDENTIFIER", currentLexeme));
-//                 resultingTokens++;
-//             }
-//             else {
-//                 // error
-//                 // pending error class
-//                 std::cerr << "Lexical Error: character: '" << line[i] << "' is not valid in the source code" << std::endl;
-//                 // i++; // Skip the invalid character
-//             }
-
-//             i = lexemeEnd;
-//         }
-//     }
-
-//     return resultingTokens;
-// }
-
 
 int Lexer::analyze(std::string line) {
     int i = 0;
@@ -176,7 +78,7 @@ int Lexer::analyze(std::string line) {
         // Find the end of the current token
         int lexemeEnd = i;
 
-        // Check if the current character is an operator or string delimiter
+        // Check if the current character is an operator
         if (std::regex_match(std::string(1, line[i]), additionOperatorRegex) ||
             std::regex_match(std::string(1, line[i]), multiplicationOperatorRegex) ||
             std::regex_match(std::string(1, line[i]), assignmentOperatorRegex) ||
@@ -189,33 +91,30 @@ int Lexer::analyze(std::string line) {
             std::regex_match(std::string(1, line[i]), leftBraceRegex) ||
             std::regex_match(std::string(1, line[i]), rightBraceRegex) ||
             std::regex_match(std::string(1, line[i]), semicolonRegex)) {
-
-            // If it's an operator, add it as a separate token
+            
+            if (std::regex_match(std::string(1, line[i]), logicalOrOperatorRegex)){
+                std::string currentOperator(1, line[i]);
+                i++;
+                if (std::regex_match(std::string(1, line[i]), logicalOrOperatorRegex)){
+                    currentOperator += line[i];
+                    this->tokens.push_back(Token("OPERATOR", currentOperator));
+                    continue;
+                }
+            }
+            else if (std::regex_match(std::string(1, line[i]), logicalAndOperatorRegex)){
+                std::string currentOperator(1, line[i]);
+                i++;
+                if (std::regex_match(std::string(1, line[i]), logicalAndOperatorRegex)){
+                    currentOperator += line[i];
+                    this->tokens.push_back(Token("OPERATOR", currentOperator));
+                    continue;
+                }
+            }
+            
             std::string currentOperator(1, line[i]);
             this->tokens.push_back(Token("OPERATOR", currentOperator));
             resultingTokens++;
             i++; // Move to the next character
-        }
-        else if (line[i] == '"') {
-            // Handle string literals
-            int stringStart = i;
-            i++; // Move past the opening double quote
-
-            while (i < lineLen && line[i] != '"') {
-                i++; // Move to the end of the string
-            }
-
-            if (i < lineLen) {
-                // Found the closing double quote
-                i++; // Move past the closing double quote
-                std::string currentString = line.substr(stringStart, i - stringStart);
-                this->tokens.push_back(Token("STRING", currentString));
-                resultingTokens++;
-            } else {
-                // Missing closing double quote
-                std::cerr << "Lexical Error: Unterminated string literal starting at position " << stringStart << std::endl;
-                break;
-            }
         }
         else {
             // Find the end of the current token (lexemeEnd)
@@ -274,6 +173,125 @@ int Lexer::analyze(std::string line) {
 
     return resultingTokens;
 }
+
+
+// int Lexer::analyze(std::string line) {
+//     int i = 0;
+//     int resultingTokens = 0;
+//     int lineLen = line.length();
+
+//     while (i < lineLen) {
+//         // Skip consecutive whitespace
+//         while (i < lineLen && isspace(line[i])) {
+//             i++;
+//         }
+
+//         if (i >= lineLen) {
+//             // Reached the end of the line, exit the loop
+//             break;
+//         }
+
+//         // Find the end of the current token
+//         int lexemeEnd = i;
+
+//         // Check if the current character is an operator or string delimiter
+//         if (std::regex_match(std::string(1, line[i]), additionOperatorRegex) ||
+//             std::regex_match(std::string(1, line[i]), multiplicationOperatorRegex) ||
+//             std::regex_match(std::string(1, line[i]), assignmentOperatorRegex) ||
+//             std::regex_match(std::string(1, line[i]), relationalOperatorRegex) ||
+//             std::regex_match(std::string(1, line[i]), logicalAndOperatorRegex) ||
+//             std::regex_match(std::string(1, line[i]), logicalOrOperatorRegex) ||
+//             std::regex_match(std::string(1, line[i]), logicalNotOperatorRegex) ||
+//             std::regex_match(std::string(1, line[i]), leftParenthesisRegex) ||
+//             std::regex_match(std::string(1, line[i]), rightParenthesisRegex) ||
+//             std::regex_match(std::string(1, line[i]), leftBraceRegex) ||
+//             std::regex_match(std::string(1, line[i]), rightBraceRegex) ||
+//             std::regex_match(std::string(1, line[i]), semicolonRegex)) {
+
+//             // If it's an operator, add it as a separate token
+//             std::string currentOperator(1, line[i]);
+//             this->tokens.push_back(Token("OPERATOR", currentOperator));
+//             resultingTokens++;
+//             i++; // Move to the next character
+//         }
+//         else if (line[i] == '"') {
+//             // Handle string literals
+//             int stringStart = i;
+//             i++; // Move past the opening double quote
+
+//             while (i < lineLen && line[i] != '"') {
+//                 i++; // Move to the end of the string
+//             }
+
+//             if (i < lineLen) {
+//                 // Found the closing double quote
+//                 i++; // Move past the closing double quote
+//                 std::string currentString = line.substr(stringStart, i - stringStart);
+//                 this->tokens.push_back(Token("STRING", currentString));
+//                 resultingTokens++;
+//             } else {
+//                 // Missing closing double quote
+//                 std::cerr << "Lexical Error: Unterminated string literal starting at position " << stringStart << std::endl;
+//                 break;
+//             }
+//         }
+//         else {
+//             // Find the end of the current token (lexemeEnd)
+//             while (lexemeEnd < lineLen && !isspace(line[lexemeEnd]) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), additionOperatorRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), multiplicationOperatorRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), assignmentOperatorRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), relationalOperatorRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), logicalAndOperatorRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), logicalOrOperatorRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), logicalNotOperatorRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), leftParenthesisRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), rightParenthesisRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), leftBraceRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), rightBraceRegex) &&
+//                 !std::regex_match(std::string(1, line[lexemeEnd]), semicolonRegex)) {
+//                 lexemeEnd++;
+//             }
+
+//             // Extract the current token as a substring
+//             std::string currentLexeme = line.substr(i, lexemeEnd - i);
+//             std::cout << "lexeme: " << currentLexeme << std::endl;
+
+//             if (std::regex_match(currentLexeme, reservedWordsRegex)) {
+//                 if (std::regex_search(currentLexeme, reservedWordsRegex)) {
+//                     this->tokens.push_back(Token("RESERVED", currentLexeme));
+//                     resultingTokens++;
+//                 } else {
+//                     // Handle the case where there's no match (error)
+//                     std::cerr << "Lexical Error: Unexpected characters starting with '" << line[i] << "'" << std::endl;
+//                     // i++; // Skip the invalid character
+//                 }
+//             }
+//             else if (std::regex_match(currentLexeme, integerRegex)) {
+//                 this->tokens.push_back(Token("INTEGER", currentLexeme));
+//                 resultingTokens++;
+//             }
+//             else if (std::regex_match(currentLexeme, realRegex)) {
+//                 this->tokens.push_back(Token("FLOAT", currentLexeme));
+//                 resultingTokens++;
+//             }
+//             else if (std::regex_match(currentLexeme, identifierRegex)) {
+//                 this->tokens.push_back(Token("IDENTIFIER", currentLexeme));
+//                 resultingTokens++;
+//             }
+//             else {
+//                 // error
+//                 // pending error class
+//                 std::cerr << "Lexical Error: character: '" << line[i] << "' is not valid in the source code" << std::endl;
+//                 // i++; // Skip the invalid character
+//             }
+
+//             i = lexemeEnd;
+//         }
+//     }
+
+//     return resultingTokens;
+// }
 
 
 
--- gui/gui.py
+++ gui/gui.py
@@ -1,11 +1,43 @@
-
-import lexer_module
-
+import tkinter as tk
+import  lexer_module
 
 lex1 = lexer_module.Lexer()
-line = "123 + 1"
-lex1.analyze(line)
-for token in lex1.tokens:
-    print(f"Token: {token.name}, value: {token.value}")
 
-# tok = lexer_module.Token("hola", "askldjfaklsj")
+def lexer():
+    output_textbox.delete("1.0", "end")
+    lex1 = lexer_module.Lexer()
+    input_text = input_textbox.get("1.0", "end-1c")
+    lex1.analyze(input_text)
+    # words = input_text.split()
+    output_textbox.delete("1.0", "end")
+    # for word in lex1.tokens:
+    #     output_textbox.insert("end", word + "\n")
+    for token in lex1.tokens:
+        output_textbox.insert("end", f"Token: {token.name}, value: {token.value}\n")
+
+def clear_textboxes():
+    input_textbox.delete("1.0", "end")
+    output_textbox.delete("1.0", "end")
+
+# Create the main window
+root = tk.Tk()
+root.title("Lexer")
+
+# Create the input textbox on the left
+input_textbox = tk.Text(root, width=40, height=10)
+input_textbox.grid(row=0, column=0, padx=10, pady=10)
+
+# Create the button in the middle
+lexer_button = tk.Button(root, text="Tokenize", command=lexer) #
+lexer_button.grid(row=1, column=0, padx=10, pady=5)
+
+# Create the clear button below the lexer button
+clear_button = tk.Button(root, text="Clear", command=clear_textboxes)
+clear_button.grid(row=2, column=0, padx=10, pady=5)
+
+# Create the output textbox on the right
+output_textbox = tk.Text(root, width=40, height=10)
+output_textbox.grid(row=0, column=1, padx=10, pady=10)
+
+# Start the Tkinter main loop
+root.mainloop()
